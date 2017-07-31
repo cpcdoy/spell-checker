c18cfa40386d90d28214107df33c200764e48b3f
Senhaji-Rhazi-Hamza <hamza.senhaji-rhazi@epita.fr>
2017-07-31 22:57:03 +0200

    test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import subprocess
+import sys
 def test(binTest, binRef, pathTest, pathRef):
    f = open(sys.argv[1], "rt")
    data = f.readlines()
@@ -15,12 +16,16 @@ def test(binTest, binRef, pathTest, pathRef):
      word =  data[rnd].split()[0]
      dist =  data[rnd].split()[1]
      argIn = "\"" + word + " " + dist + "\""
-     echo =  subprocess.run(["echo", argIn], stdout=subprocess.PIPE)
-     rt = subprocess.run(args = argsTest, stdin = echo.stdout,  stdout=subprocess.PIPE)
-     rr = subprocess.run(args = argsRef, stdin = echo.stdout,  stdout=subprocess.PIPE)
+     #echo =  subprocess.run(["echo", argIn],  stdout=subprocess.PIPE)
+     std = subprocess.Popen(["echo", argIn], stdout=subprocess.PIPE, shell=True)
+     std.wait()
+     rt = subprocess.run(args = argsTest, stdin =std.stdout,   stdout=subprocess.PIPE)
+     rr = subprocess.run(args = argsRef, stdin =std.stdout,  stdout=subprocess.PIPE)
      resTest = rt.stdout # other version resTest += rt.stdout
      resRef = rr.stdout  # other version resRef += rr.stdout
+     print(resTest)
+     print((resTest == resRef))
      note[i] = int(resTest == resRef)
    print('your score is ', note.mean(), " on 100")
 
-test("TextMiningApp", "subject/ref/TextMiningApp", "dict.bin", "subject/dict.bin")
+test("./TextMiningApp", "./TextMiningAppRef", "dic.bin", "dicRef.bin")
